7a5973bd5cae0e704035f112f79a6c33dfd907d4
johbrodin <johbrodin@gmail.com>
2017-12-06 10:33:10 +0100

    Add files via upload

---
+++ mgPrecondition.h
@@ -0,0 +1,405 @@
+
+/*-----------------------------------
+- GLT-MG; implementation in deal.ii 
+- by Hreinn Juliusson aut.17
+-------------------------------------*/
+
+#ifndef mgPreconditionition_H
+#define mgPreconditionition_H
+
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+#include <deal.II/base/config.h>
+#include <deal.II/base/smartpointer.h>
+#include <deal.II/base/utilities.h>
+#include <deal.II/base/parallel.h>
+#include <deal.II/base/template_constraints.h>
+#include <deal.II/base/thread_management.h>
+#include <deal.II/lac/diagonal_matrix.h>
+#include <deal.II/lac/solver_cg.h>
+#include <deal.II/lac/vector_memory.h>
+
+#include <deal.II/lac/sparse_matrix.h>
+#include <deal.II/lac/dynamic_sparsity_pattern.h>
+#include <deal.II/numerics/vector_tools.h>
+#include <deal.II/numerics/matrix_tools.h>
+#include <deal.II/lac/vector.h>
+#include <deal.II/lac/full_matrix.h>
+#include <deal.II/lac/identity_matrix.h> // IdentityMatrix inclusion :)
+
+DEAL_II_NAMESPACE_OPEN
+//using namespace mgPrecondition;
+
+class mgPrecondition: public Subscriptor{
+
+public:
+	mgPrecondition(const SparseMatrix<double> &sparse_matrix,
+		const Vector<double> &vector);
+	void vmult(Vector<double> &dst, const Vector<double> &src) const;
+	//GLTmg methods remove some for optimization
+	void presmooth(Vector<double> &vicky);
+	void postsmooth(Vector<double> &vicky);
+	/* These method are used outside of this class */
+	//Vector<int> factor(const int N);
+	//Vector<int> unique(Vector<int> factor);
+	//Vector<int> accumVector(Vector<int> v);
+	//double vectorProd(Vector<int> v1, Vector<int> v2);
+	void prol(double n, SparsityPattern &spP, SparseMatrix<double> &smP); // Change the return type ;)
+	void kronProd(SparseMatrix<double> &A, SparseMatrix<double> &B,
+		SparsityPattern &sp, SparseMatrix <double> &M);
+	void kronProd_vector(Vector<double> &A, Vector<double> &B,
+		SparsityPattern &sp, SparseMatrix <double> &M);
+	void spdiags(double n,SparsityPattern &spaa, SparseMatrix <double> &aa);
+	// Help methods
+	void printMatrix();
+	void printVector();
+	void sayHi();
+private:
+	const SmartPointer<const SparseMatrix<double>> system_matrix;
+	const SmartPointer<const Vector<double>> rhs;
+	SmartPointer<Vector<double>> test_vector;
+	SmartPointer<SparseMatrix<double>> test_matrix;
+};
+
+mgPrecondition::mgPrecondition(const SparseMatrix<double> &sparse_matrix,
+	const Vector<double> &vector):
+system_matrix(&sparse_matrix),
+rhs(&vector)
+{}
+
+// This will be the GLTmg main function!
+void mgPrecondition::vmult(Vector<double> &dst, const Vector<double> &src) const {
+	dst = src;
+	std::cout<<" - - - - - - Ding - - - - - - -"<<std::endl;
+}
+
+/* ===================== The functions needed to perform GLTmg ==============================================================*/
+
+/* Presmoot alg.
+	diagA = diag(A);
+	x = x+1*(b-A*x).diagA;
+	We use the predefined Jacobi_step
+*/
+void mgPrecondition::presmooth(Vector<double> &vicky){
+	system_matrix->Jacobi_step(vicky,*rhs,1); // Remember smartpointer to rhs!
+}
+
+void mgPrecondition::postsmooth(Vector<double> &vicky){
+	const double damp = 2./3;
+	system_matrix->Jacobi_step(vicky,*rhs,damp);
+}
+
+/* Return a vector containing the primenumbers of N 
+	Might Implement Sieve at a later point 		*/
+Vector<int> factor(int N){
+	int index = 0;
+	const int maxSize = 10;
+	int factors[maxSize];
+	while(N%2==0){
+		//Add 2
+		factors[index] = 2;
+		index++;
+		N=N/2;
+	}
+	for(int i=3; i<=sqrt(N); i=i+2){
+		while(N%i==0){
+			//add i
+			factors[index] = i;
+			index++;
+			N=N/i;
+		}
+	}
+	if(N>2){
+		//add (N)
+		factors[index] = N;
+		index++;
+	}
+	Vector<int> result;
+	result.reinit(index);
+	Vector<int>::iterator iter = result.begin();
+	Vector<int>::iterator ender = result.end();
+	int i = 0;
+	for(; iter!=ender; iter++){
+		*iter = factors[i];
+		i++;
+	}
+	return result;
+}
+
+/* removes extra numbers from factors */
+Vector<int> unique(Vector<int> factor){
+	if(factor.size()==1){
+		return factor;
+	} else{
+		Vector<int> unique;
+		const int maxSize = 10;
+		int tmp[maxSize];
+		int i = 0;
+		Vector<int>::iterator iter = factor.begin();
+		Vector<int>::iterator ender = factor.end();
+		tmp[i] = *iter;
+		iter++;
+		for(; iter!=ender; iter++){
+			if(*iter!=tmp[i]){
+				i++;
+				tmp[i] = *iter;
+			}
+		}
+		unique.reinit(i+1);
+		iter = unique.begin();
+		ender = unique.end();
+		for(i=0;iter!=ender; iter++){
+			*iter = tmp[i];
+			i++;
+		}
+		return unique;
+	}
+}
+
+/* Returns an accumVector. This method assumes v is sorted! 
+	Note! The GLTmg has *ugh* Matlab indexing !
+	Returnes # of Values of pos int on index i !
+*/
+Vector<int> accumVector(Vector<int> v){
+	Vector<int>::iterator iter = v.begin();
+	Vector<int>::iterator ender = v.end();
+	Vector<int> res;
+	int res_size = *(ender-1);
+	res.reinit(res_size); //prolong so you can access the last element!
+	res = 0;
+	for(; iter!=ender; iter++){
+		res(*iter-1)++;
+	}
+	return res;
+}
+
+/* This method performs the prod(nu.^(resp(nu)./3)) 
+	action given in the GLTmg*/
+double vectorProd(Vector<int> reps, Vector<int> nu){
+	double n = 1;
+	// the reps(nu) action
+	Vector<double> repsNu;	//Used for development
+	repsNu.reinit(nu.size());
+	Vector<int>::iterator nu_iter = nu.begin();
+	Vector<int>::iterator nu_ender = nu.end();
+	int i =  0;
+	for(; nu_iter!=nu_ender; nu_iter++){
+		repsNu(i) = (double)(reps(*nu_iter-1))/3;
+		n = n*pow(nu(i),repsNu(i));
+		i++;
+	}
+	return n;
+}
+/*
+	This is the kronecker product of 2 sparse matrices
+*/
+void mgPrecondition::kronProd(SparseMatrix<double> &A, SparseMatrix<double> &B,
+	SparsityPattern &sp, SparseMatrix <double> &M){
+	const int nA = A.n(); //nCols
+	const int nB = B.n();
+	const int mA = A.m(); //nRows
+	const int mB = B.m();
+	const int n = nA*nB;
+	const int m = mA*mB;
+	DynamicSparsityPattern dsp(m,n);
+	SparseMatrix<double>::iterator itA = A.begin();
+	SparseMatrix<double>::iterator endA = A.end();
+	SparseMatrix<double>::iterator itB = B.begin();	// I fixed it ;P 
+	SparseMatrix<double>::iterator endB = B.end();
+	int rowA, colA, rowB, colB;
+	int i,j;
+	double val,valA,valB;
+	for(; itA!=endA; itA++){
+		rowA = itA->row();
+		colA = itA-> column();
+		for(; itB!=endB; itB++){
+			rowB = itB->row();
+			colB = itB-> column();
+			i = rowA*mB+rowB;
+			j = colA*nB+colB;
+			dsp.add(i,j);
+		}
+		itB = B.begin();
+	}
+	dsp.compress();
+	// Should we put a sp.clear?
+	sp.copy_from(dsp);
+	M.reinit(sp);
+	itA = A.begin();
+	for(; itA!=endA; itA++){
+		rowA = itA->row();
+		colA = itA-> column();
+		valA = itA->value();
+		for(; itB!=endB; itB++){
+			rowB = itB->row();
+			colB = itB-> column();
+			valB = itB->value();
+			i = rowA*mB+rowB;
+			j = colA*nB+colB;
+			val = valA*valB;
+			M.add(i,j,val);
+		}
+		itB = B.begin();
+	}
+}
+
+/* Vector A is percieved to be a 1xn vector
+and Vector B is percieved to be a mx1 vector!
+	resulting Matrix M is a mxn matrix
+*/
+void mgPrecondition::kronProd_vector(Vector<double> &A, Vector<double> &B,
+	SparsityPattern &sp, SparseMatrix <double> &M){
+	const int m = B.size();
+	const int n = A.size();
+	DynamicSparsityPattern dsp(m,n);
+	Vector<double>::iterator itA = A.begin();
+	Vector<double>::iterator itB = B.begin();
+	Vector<double>::iterator endA = A.end();
+	Vector<double>::iterator endB = B.end();
+	int i=0,j=0;
+	double val,valA,valB;
+	for(; itA!=endA; itA++){
+		for(; itB!=endB; itB++){
+			dsp.add(j,i);
+			j++;
+		}
+		itB = B.begin();
+		j=0;
+		i++;
+	}
+	dsp.compress();
+	sp.copy_from(dsp);
+	M.reinit(sp);
+	itA = A.begin();
+	i=0; j=0;
+	for(; itA!=endA; itA++){
+		valA = *itA;
+		for(; itB!=endB; itB++){
+			valB = *itB;
+			val = valA*valB;
+			M.add(j,i,val);
+			j++;
+		}
+		itB = B.begin();
+		j=0;
+		i++;
+	}
+}
+
+/* Here we implement the prol([2 1 1],[0 -1 1],n)
+	dd = [0 -1 1]
+	PP = [2 1 1]
+	performs the operation:
+	aa = spdiags(kron(PP,one(n,1),dd,n,n));
+*/
+void mgPrecondition::spdiags(double n,SparsityPattern &spaa, SparseMatrix <double> &aa){
+	Vector<double> dd;
+	dd.reinit(3);
+	dd[0] = 0; dd[1] = -1; dd[2] = 1;
+	Vector<double> PP;
+	PP.reinit(3);
+	PP[0] = 2; PP[1] = 1; PP[2] = 1; // Obs!!!
+	int N = (int)n;
+	/* Make the sm1 = kron(PP,ones(n,1)) matrix! :D */
+	Vector<double> ones;
+	ones.reinit(N);
+	ones = 1;
+	SparsityPattern sp1;
+	SparseMatrix<double> sm1;
+	kronProd_vector(PP,ones,sp1,sm1);
+	/* I am hardcoding the diagonals. i.e. I dont use the dd matrix. Sorry ;)*/
+	DynamicSparsityPattern dsp(N,N);
+	// ! ! ! ! !  Possible problem if the matrix is shorter than N !! ! ! !
+	for(int i=0; i<N-1; i++){
+		dsp.add(i,i);
+		dsp.add(i+1,i);
+		dsp.add(i,i+1);
+	}
+	dsp.add(N-1,N-1);
+	dsp.compress();
+	spaa.copy_from(dsp);
+	aa.reinit(spaa);
+	for(int i=0; i<N-1; i++){
+		aa.add(i,i,sm1(i,0));		// Column 0 on diag 0
+		aa.add(i+1,i,sm1(i+1,1));	// Column 1 on diag -1
+		aa.add(i,i+1,sm1(i,2));		// Column 2 on diag 1
+	}
+	aa.add(N-1,N-1,sm1(N-1,0));
+}
+
+/* This is an implementation of the algorithm 
+	(1/n)*prol([2 1 1], [0 -1 1],n)
+	given in the gltmg_test matlab code 
+	*/
+void mgPrecondition::prol(double n, SparsityPattern &spP, SparseMatrix<double> &smP){
+	SparsityPattern spaa;
+	SparseMatrix<double> aa;
+	spdiags(n,spaa,aa);
+	SparsityPattern spTemp;
+	SparseMatrix<double> smTemp;
+	kronProd(aa,aa,spTemp,smTemp);
+	kronProd(aa,smTemp,spP,smP);
+	/* Create H = speye(n) then remove every other row
+		H = H(1:2:n,:)*/
+	SparsityPattern spH;
+	SparseMatrix<double> smH;
+	int N1 = (int)n;
+	int N2 = N1/2+N1%2;
+	DynamicSparsityPattern dspH(N2,N1);
+	for(int i=0; i<N2; i++){
+		dspH.add(i,2*i);
+	}
+	dspH.compress();
+	spH.copy_from(dspH);
+	smH.reinit(spH);
+	for(int i=0; i<N2; i++){
+		smH.add(i,2*i,1);
+	}
+	// H = kron(H,H)
+	SparsityPattern spH2;
+	SparseMatrix<double> H;
+	kronProd(smH,smH,spH2,H);
+	H.print_formatted(std::cout,1,true,0," ",1);
+	
+}
+
+
+/*==========================================================================================================================*/
+
+/* Test/Help functions! */
+void mgPrecondition::sayHi(){
+	std::cout<<" - - - - Hi There! - - - -  "
+	<<std::endl;
+}
+void mgPrecondition::printMatrix(){
+	//Why do we use arrow here? -> = (*).
+	system_matrix->print_formatted(std::cout,1,true,0," ",1);
+}
+void mgPrecondition::printVector(){
+	rhs->print(std::cout,1,true,true);	
+}
+
+void foo(int &N){
+	N+=30;
+}
+
+
+DEAL_II_NAMESPACE_CLOSE
+#endif
+
+
+
+/*
+
+ __
+ | |
+ | |
+ | |
+ | |
+ | |		   _____
+ | |		  |		|	  |
+ | | Hreinn	  |		|	  |
+_| |__________|		|_____|_____
+*/
---
+++ step-3-plain.cc
@@ -0,0 +1,397 @@
+/* ---------------------------------------------------------------------
+ *
+ * Copyright (C) 1999 - 2016 by the deal.II authors
+ *
+ * This file is part of the deal.II library.
+ *
+ * The deal.II library is free software; you can use it, redistribute
+ * it, and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * The full text of the license can be found in the file LICENSE at
+ * the top level of the deal.II distribution.
+ *
+ * ---------------------------------------------------------------------
+ *
+ * Authors: Wolfgang Bangerth, 1999,
+ *          Guido Kanschat, 2011
+ */
+#include <deal.II/grid/tria.h>
+#include <deal.II/dofs/dof_handler.h>
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/tria_accessor.h>
+#include <deal.II/grid/tria_iterator.h>
+#include <deal.II/dofs/dof_accessor.h>
+#include <deal.II/fe/fe_q.h>
+#include <deal.II/dofs/dof_tools.h>
+#include <deal.II/fe/fe_values.h>
+#include <deal.II/base/quadrature_lib.h>
+#include <deal.II/base/function.h>
+#include <deal.II/numerics/vector_tools.h>
+#include <deal.II/numerics/matrix_tools.h>
+#include <deal.II/lac/vector.h>
+#include <deal.II/lac/full_matrix.h>
+#include <deal.II/lac/sparse_matrix.h>
+#include <deal.II/lac/dynamic_sparsity_pattern.h>
+#include <deal.II/lac/solver_cg.h>
+#include <deal.II/lac/precondition.h>
+#include <deal.II/numerics/data_out.h>
+#include <fstream>
+#include <iostream>
+#include <vector> // This is for the std::vector
+//#include <deal.II/multigrid/mg_matrix.h> // For the mg::Matrix ;)
+
+
+//#include "helloHeader.h"
+//#include "helloPrecondition.h"
+#include "mgPrecondition.h"
+
+using namespace dealii;
+class Step3
+{
+public:
+  Step3 ();
+  void run ();
+  // Added Public methods
+  void test_run(); // Here we run our tests!
+  void testProl();
+  void mgGLT_init();
+private:
+  void make_grid ();
+  void setup_system ();
+  void assemble_system ();
+  void solve ();
+  void output_results () const;
+  /*Added private methods */
+  void inputFile(unsigned int size1, unsigned int size2,std::string filename);
+  void inputFile_supplied(unsigned int, unsigned int,std::string,SparsityPattern &sp, SparseMatrix<double> &sm);
+  void writeToFile(SparseMatrix<double> &matrix,std::string filename);
+  void canITouchThis(SparseMatrix<double> &M, SparsityPattern &sp);
+
+  Triangulation<2>     triangulation;
+  FE_Q<2>              fe;
+  DoFHandler<2>        dof_handler;
+  SparsityPattern      sparsity_pattern;
+  SparseMatrix<double> system_matrix;
+  Vector<double>       solution;
+  Vector<double>       system_rhs;
+};
+Step3::Step3 ()
+:
+fe (1),
+dof_handler (triangulation)
+{}
+void Step3::make_grid ()
+{
+  GridGenerator::hyper_cube (triangulation, -1, 1);
+  triangulation.refine_global (5);
+  std::cout << "Number of active cells: "
+  << triangulation.n_active_cells()
+  << std::endl;
+}
+void Step3::setup_system ()
+{
+  dof_handler.distribute_dofs (fe);
+  std::cout << "Number of degrees of freedom: "
+  << dof_handler.n_dofs()
+  << std::endl;
+  DynamicSparsityPattern dsp(dof_handler.n_dofs());
+  DoFTools::make_sparsity_pattern (dof_handler, dsp);
+  sparsity_pattern.copy_from(dsp);
+  system_matrix.reinit (sparsity_pattern);
+  solution.reinit (dof_handler.n_dofs());
+  system_rhs.reinit (dof_handler.n_dofs());
+}
+void Step3::assemble_system ()
+{
+  QGauss<2>  quadrature_formula(2);
+  FEValues<2> fe_values (fe, quadrature_formula,
+   update_values | update_gradients | update_JxW_values);
+  const unsigned int   dofs_per_cell = fe.dofs_per_cell;
+  const unsigned int   n_q_points    = quadrature_formula.size();
+  FullMatrix<double>   cell_matrix (dofs_per_cell, dofs_per_cell);
+  Vector<double>       cell_rhs (dofs_per_cell);
+  std::vector<types::global_dof_index> local_dof_indices (dofs_per_cell);
+  DoFHandler<2>::active_cell_iterator cell = dof_handler.begin_active();
+  DoFHandler<2>::active_cell_iterator endc = dof_handler.end();
+  for (; cell!=endc; ++cell)
+  {
+    fe_values.reinit (cell);
+    cell_matrix = 0;
+    cell_rhs = 0;
+    for (unsigned int q_index=0; q_index<n_q_points; ++q_index)
+    {
+      for (unsigned int i=0; i<dofs_per_cell; ++i){
+        for (unsigned int j=0; j<dofs_per_cell; ++j){
+          cell_matrix(i,j) += (fe_values.shape_grad (i, q_index) *
+           fe_values.shape_grad (j, q_index) *
+           fe_values.JxW (q_index));
+        }
+      }
+      for (unsigned int i=0; i<dofs_per_cell; ++i){
+        cell_rhs(i) += (fe_values.shape_value (i, q_index) *
+          1 *
+          fe_values.JxW (q_index));
+      }
+    }
+    cell->get_dof_indices (local_dof_indices);
+    for (unsigned int i=0; i<dofs_per_cell; ++i){
+      for (unsigned int j=0; j<dofs_per_cell; ++j){
+        system_matrix.add (local_dof_indices[i],
+         local_dof_indices[j],
+         cell_matrix(i,j));
+      }
+    }
+    for (unsigned int i=0; i<dofs_per_cell; ++i){
+      system_rhs(local_dof_indices[i]) += cell_rhs(i);
+    }
+  }
+  std::map<types::global_dof_index,double> boundary_values;
+  VectorTools::interpolate_boundary_values (dof_handler,
+    0,
+    ZeroFunction<2>(),
+    boundary_values);
+  MatrixTools::apply_boundary_values (boundary_values,
+    system_matrix,
+    solution,
+    system_rhs);
+}
+void Step3::solve ()
+{
+      /* Original Step3::solve */ 
+  SolverControl           solver_control (1000, 1e-12);
+  SolverCG<>              solver (solver_control);
+  solver.solve (system_matrix, solution, system_rhs,
+    mgPrecondition(system_matrix,system_rhs));
+  
+}
+void Step3::output_results () const
+{
+  DataOut<2> data_out;
+  data_out.attach_dof_handler (dof_handler);
+  data_out.add_data_vector (solution, "solution");
+  data_out.build_patches ();
+  std::ofstream output ("solution.gpl");
+  data_out.write_gnuplot (output);
+}
+void Step3::run ()
+{
+      /* Original Step3::run */
+  make_grid ();
+  setup_system ();
+  assemble_system ();
+  solve ();
+  output_results ();
+}
+
+  /*======================================== Added Methods for tests ======================================================*/
+
+       /* Read from input file ! !
+        Adjust for writing on new sparsity pattern and Sparse*/
+void Step3::inputFile(unsigned int size1, unsigned int size2,std::string filename){
+
+  DynamicSparsityPattern dsp(size1,size2);
+  std::ifstream input_file(filename);
+  unsigned int i,j;
+  double val;
+
+  if(!input_file){
+    std::cout << "While opening file, error encounterde"<<std::endl;
+  }
+  else{
+    std::cout << "File is successfully opened" << std::endl;
+  }
+  while(input_file>>i>>j>>val)//(!input_file.eof())
+  {
+    dsp.add(i-1,j-1);
+  }
+  input_file.close();
+  dsp.compress();
+  sparsity_pattern.copy_from(dsp);
+  system_matrix.reinit(sparsity_pattern);
+
+  std::ifstream input_file_again(filename);
+  if(!input_file_again){
+    std::cout << "While opening a file an error is encountered" << std::endl;
+  }
+  else{
+    std::cout << "File is successfully opened" << std::endl;
+  }
+  while(input_file_again>>i>>j>>val)//(!input_file.eof())
+  {
+    system_matrix.add(i-1,j-1,val);
+  }
+  input_file_again.close();
+
+}
+
+void Step3::inputFile_supplied(unsigned int size1, unsigned int size2,std::string filename, 
+  SparsityPattern &sp, SparseMatrix<double> &sm){
+
+  DynamicSparsityPattern dsp(size1,size2);
+  std::ifstream input_file(filename);
+  unsigned int i,j;
+  double val;
+
+  if(!input_file){
+    std::cout << "While opening file, error encounterde"<<std::endl;
+  }
+  else{
+    //std::cout << "File is successfully opened" << std::endl;
+  }
+  while(input_file>>i>>j>>val)//(!input_file.eof())
+  {
+    dsp.add(i-1,j-1);
+  }
+  input_file.close();
+  dsp.compress();
+  sp.copy_from(dsp);
+  sm.reinit(sp);
+
+  std::ifstream input_file_again(filename);
+  if(!input_file_again){
+    std::cout << "While opening a file an error is encountered" << std::endl;
+  }
+  else{
+    //std::cout << "File is successfully opened" << std::endl;
+  }
+  while(input_file_again>>i>>j>>val)//(!input_file.eof())
+  {
+    sm.add(i-1,j-1,val);
+  }
+  input_file_again.close();
+}
+
+void Step3::writeToFile(SparseMatrix<double> &matrix,std::string filename){
+  // filename ex "matrix.txt"
+  std::ofstream out_system (filename);
+  matrix.print_formatted(out_system,3,true,0," ",1); 
+}
+
+/* Just a method to test the reference operator & */
+void Step3::canITouchThis(SparseMatrix<double> &M,SparsityPattern &sp){
+  inputFile_supplied(3,3,"bb.txt",sp,M);
+}
+
+// This method runs our tests !
+void Step3::test_run(){
+  /* This writes the values from "bb.txt" to SM*/
+  SparsityPattern      SP;
+  SparseMatrix<double> A;
+  Vector<double> b;
+  int size = 3;
+  b.reinit(size);
+  b = 5;
+  inputFile_supplied(size,size,"bb.txt",SP,A);
+  mgPrecondition mgTest(A,b);
+}
+
+void Step3::testProl(){
+  SparsityPattern      SP;
+  SparseMatrix<double> sys;
+  Vector<double> rhs;
+  int size = 3;
+  rhs.reinit(size);
+  rhs = 1;
+  inputFile_supplied(size,size,"bb.txt",SP,sys);
+  mgPrecondition mgTest(sys,rhs); // mgCreation
+  SparsityPattern      spP;
+  SparseMatrix<double> smP;
+  double n = 6;
+  mgTest.prol(n,spP,smP);
+  int buzz = 20;
+  foo(buzz);
+  std::cout<<" - - - - -: "<<buzz<<std::endl;
+
+}
+
+/* Pseudo Code for running the tests ! */
+// I should put some methods not as mgPrecond classes! ! !
+void Step3::mgGLT_init(){
+  // for j=2:6 levels
+  SparsityPattern spAFin;
+  SparseMatrix<double> AFin;
+  int size = 3;
+  inputFile_supplied(size,size,"bb.txt",spAFin,AFin);
+  int N = size; // N could be an int, AFin quadratic.
+  Vector<double> y;
+  y.reinit(N);
+  y = 1;
+  Vector<double> b;
+  AFin.vmult(b,y);
+
+  
+  Vector<int> NN = factor(N);
+  Vector<int> nu = unique(NN);
+  Vector<int> reps = accumVector(NN);
+  double n = vectorProd(reps,nu);
+
+
+
+}
+
+/*========================================================================================================================*/
+
+int main ()
+{
+  //deallog.depth_console (2);
+  //Step3 laplace_problem;
+  //laplace_problem.run ();
+  Step3 test;
+  test.run();
+  //tests.testProl();  
+
+  return 0;
+}
+/* - - - - - -  The code-dump - - - - - - - - - */
+
+/* - - - - - Here I test BB and PP storages - - - - - - -*/
+  /* Not sure if this is sound memory handling! ! ! 
+      But this works just fine . . . . 
+  std::vector<SparseMatrix<double>*> vicky;
+  vicky.push_back(&A); vicky.push_back(&B);
+  vicky[0]->print_formatted(std::cout,1,true,0," ",1);
+  */
+
+  /* Could use MGLevelObject<Object>, which actually referenses the std::vector!
+    Cant find the method to add shit :/
+
+  MGLevelObject<SparseMatrix <double>> list();
+  list.add(A);*/
+
+
+  /* Part of the GLTmg Run 
+  Vector<int> NN = mgTest.factor(N);
+  Vector<int> nu = mgTest.unique(NN);
+  Vector<int> reps = mgTest.accumVector(NN);
+  std::cout<<" NN:   "; NN.print(std::cout,3,true,true);
+  std::cout<<" nu:   "; nu.print(std::cout,3,true,true);
+  std::cout<<" reps: "; reps.print(std::cout,3,true,true);
+  double n = mgTest.vectorProd(reps,nu);
+  std::cout<<"n : "<<n<<std::endl;
+  */
+
+/*
+   SM.print_formatted(std::cout,1,true,0," ",1);
+  writeToFile(SM,"matrix.txt");
+ SparsityPattern::iterator start = SP.begin();
+
+  system_matrix.print_formatted(std::cout,3,true,0," ",1);
+  system_rhs.print(std::cout,3,true,true);
+  inputFile(3,3,"bb.txt");
+  mgPrecondition mgPre1(system_matrix,system_rhs);
+  mgPre1.printMatrix();
+  system_matrix.print_formatted(std::cout,1,true,0," ",1);
+  mgPre1.printVector();
+
+  Vector<double> test_vector;
+  canITouchThis(test_vector);
+  //test_vector.print(std::cout,3,true,true);
+  Vector<double>::iterator iter = b.begin();
+  Vector<double>::iterator ender = b.end();
+  for(; iter!=ender; ++iter){
+    std::cout<<*iter<<std::endl;
+    *iter = 10;
+  }
+  b.print(std::cout,1,true,true);
+  */
